6bbaa7914258ab973925e96a8b2904851db69bca
LaurencePortron <portron.laurence@gmail.com>
2020-10-30 09:45:39 +0100

    self made carousel, first iteration

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -1,35 +1,114 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
+/* eslint-disable react/no-string-refs */
+import React, { Component } from 'react';
+import './Carousel.css';
+import data from './data.json';
+import Slide from './Slide';
+import scrollToAnimate from './scrollToAnimate';
 
-export default () => (
-  <Carousel>
-    <div>
-      <img
-        alt=""
-        src="https://www.sciencenewsforstudents.org/wp-content/uploads/2019/11/061217_LG_jupiter_main_free.jpg"
-      />
-      <p className="legend">Jupiter</p>
-    </div>
-    <div>
-      <img
-        alt=""
-        src="https://www.nasa.gov/sites/default/files/1-bluemarble_west.jpg "
-      />
-      <p className="legend">Earth</p>
-    </div>
-    <div>
-      <img
-        alt=""
-        src="https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg"
-      />
-      <p className="legend">Mars</p>
-    </div>
-    <div>
-      <img
-        alt=""
-        src="https://www.popsci.com/resizer/W7jnUQi0KvvKIqLkoL24ForVX54=/760x570/cloudfront-us-east-1.images.arcpublishing.com/bonnier/GVRV4HR4H5GGBPEEDUPVC6ZG4Q.jpg"
-      />
-      <p className="legend">Moon</p>
-    </div>
-  </Carousel>
-);
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLeftNavigation = this.handleLeftNavigation.bind(this);
+    this.onResize = this.onResize.bind(this);
+    this.obj = data;
+    this.state = {
+      numberOfSlidesToScroll: 4,
+    };
+  }
+
+  componentDidMount() {
+    this.checkNumberOfSlidesToScroll();
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize() {
+    console.log('resizing', this);
+  }
+
+  handleRightNavigation = () => {
+    const { carouselViewport } = this.refs;
+    const { numberOfSlidesToScroll } = this.state;
+    const widthOfSlide = 120;
+    const newPosition =
+      carouselViewport.scrollLeft + numberOfSlidesToScroll * widthOfSlide;
+    const timeToMoveOneSlide = 200;
+    const totalTimeToMove = Math.min(
+      numberOfSlidesToScroll * timeToMoveOneSlide,
+      400
+    );
+    scrollToAnimate({
+      element: carouselViewport,
+      to: newPosition,
+      duration: totalTimeToMove,
+      scrollDirection: 'scrollLeft',
+    });
+  };
+
+  checkNumberOfSlidesToScroll() {
+    let numberOfSlidesToScroll;
+    if (window.innerWidth <= 900) {
+      numberOfSlidesToScroll = 1;
+    } else {
+      numberOfSlidesToScroll = 4;
+    }
+    // eslint-disable-next-line react/destructuring-assignment
+    if (this.state.numberOfSlidesToScroll !== numberOfSlidesToScroll) {
+      this.setState({ numberOfSlidesToScroll });
+    }
+  }
+
+  handleLeftNavigation() {
+    const { carouselViewport } = this.refs;
+    const { numberOfSlidesToScroll } = this.state;
+    const widthOfSlide = 120;
+    const newPosition =
+      carouselViewport.scrollLeft - numberOfSlidesToScroll * widthOfSlide;
+    const timeToMoveOneSlide = 200;
+    const totalTimeToMove = Math.min(
+      numberOfSlidesToScroll * timeToMoveOneSlide,
+      400
+    );
+    scrollToAnimate({
+      element: carouselViewport,
+      to: newPosition,
+      duration: totalTimeToMove,
+      scrollDirection: 'scrollLeft',
+    });
+  }
+
+  renderSlides() {
+    return this.obj.map((item) => {
+      return <Slide name={item.name} key={item.id} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="carousel-container">
+        <button
+          className="carousel-nav carousel-left-navigation-button"
+          onClick={this.handleLeftNavigation}
+          type="button"
+        >
+          &#60;
+        </button>
+        <div className="carousel-viewport" ref="carouselViewport">
+          {this.renderSlides()}
+        </div>
+        <button
+          className="carousel-nav carousel-right-navigation-button"
+          onClick={this.handleRightNavigation}
+          type="button"
+        >
+          &#62;
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
---
+++ src/components/home/Slide.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Slide.css';
+
+const Slide = ({ name }) => {
+  return <div className="slide">{name}</div>;
+};
+
+export default Slide;
