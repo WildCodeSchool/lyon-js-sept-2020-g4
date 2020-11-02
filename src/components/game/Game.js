b287586d3afe537cdcc58f6a67070e32e313220f
Aymeric <aymeric.bouault@gmail.com>
2020-11-02 16:01:12 +0100

    create components Rules and Characters and imports management

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Rules from './Rules';
+import Characters from './Characters';
 
 function Game() {
   return (
     <div>
       <h1>Game</h1>
+      <Rules />
+      <Characters />
     </div>
   );
 }
---
+++ src/components/game/Rules.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Rules extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>123</div>;
+  }
+}
+
+export default Rules;
