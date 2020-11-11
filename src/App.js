9bade10a0bd62e9644a1ab1535cc8dbe6bec5851
AntoineGGG <a.gorenflot@gmx.fr>
2020-11-11 08:02:24 +0100

    Finalisation Moteur Engine OK

--- src/App.js
+++ src/App.js
@@ -11,62 +11,61 @@ import { GameProvider } from './components/game/GameContext';
 
 export default function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <div className="toggle">
-              <input type="checkbox" />
-              <span />
-              <span />
-              <span />
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/game">Game</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/start" component={Start} />
-            <GameProvider>
+    <GameProvider>
+      <div className="App">
+        <Router>
+          <div>
+            <nav>
+              <div className="toggle">
+                <input type="checkbox" />
+                <span />
+                <span />
+                <span />
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/game">Game</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/start" component={Start} />
               <Route path="/game" component={Game} />
-            </GameProvider>
-
-            <Route path="/about">
-              <FormValidation />
-              <About
-                name="Romain"
-                description="je suis un membre de la Wild Code School"
-                avatar="https://randomuser.me/api/portraits/men/40.jpg"
-              />
-              <About
-                name="Laurence"
-                description="je suis un membre de la Wild Code School"
-                avatar="astro.jpg"
-              />
-              <About
-                name="Antoine"
-                description="je suis un membre de la Wild Code School"
-                avatar="https://randomuser.me/api/portraits/men/22.jpg"
-              />
-              <About
-                name="Aymeric"
-                description="je suis un membre de la Wild Code School"
-                avatar="https://randomuser.me/api/portraits/men/61.jpg"
-              />
-            </Route>
-            <Route exact path="/:id" component={PlanetsDetails} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+              <Route path="/about">
+                <FormValidation />
+                <About
+                  name="Romain"
+                  description="je suis un membre de la Wild Code School"
+                  avatar="https://randomuser.me/api/portraits/men/40.jpg"
+                />
+                <About
+                  name="Laurence"
+                  description="je suis un membre de la Wild Code School"
+                  avatar="astro.jpg"
+                />
+                <About
+                  name="Antoine"
+                  description="je suis un membre de la Wild Code School"
+                  avatar="https://randomuser.me/api/portraits/men/22.jpg"
+                />
+                <About
+                  name="Aymeric"
+                  description="je suis un membre de la Wild Code School"
+                  avatar="https://randomuser.me/api/portraits/men/61.jpg"
+                />
+              </Route>
+              <Route exact path="/:id" component={PlanetsDetails} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </GameProvider>
   );
 }
