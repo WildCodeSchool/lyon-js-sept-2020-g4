64a4a2d3982cfe2d7cafd0327c6fe52015f45200
LaurencePortron <portron.laurence@gmail.com>
2020-11-11 12:08:47 +0100

    difference of time function set

--- src/components/home/SmallRocket.js
+++ src/components/home/SmallRocket.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-
 import axios from 'axios';
 
 const SmallRocket = () => {
@@ -10,9 +9,22 @@ const SmallRocket = () => {
       .get(`https://api.spacexdata.com/v3/launches/next`)
       .then((response) => {
         setNextLaunch(response.data.launch_date_utc);
-        // console.log(response.data.launch_date_local);
+        // console.log(response.data.launch_date_utc);
       });
   }, [nextLaunch]);
+
+  const launchDate = '2020-11-15T00:49:00.000Z';
+  const calculateTimeLeft = () => {
+    const difference = Date.parse(launchDate) - Date.parse(new Date());
+    const seconds = Math.floor((difference / 1000) % 60);
+    const minutes = Math.floor((difference / 1000 / 60) % 60);
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    return { difference, days, hours, minutes, seconds };
+  };
+  calculateTimeLeft();
+  console.log(calculateTimeLeft());
 
   return (
     <div>
