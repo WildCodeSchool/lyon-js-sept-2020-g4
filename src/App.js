6a46afd0382c017c9d8d1cf11555e4c31f380fd4
AntoineGGG <a.gorenflot@gmx.fr>
2020-11-11 14:03:12 +0100

    Creation start context pour gerer l'état de la fenetre de bienvenue

--- src/App.js
+++ src/App.js
@@ -8,45 +8,48 @@ import Start from './components/home/Start';
 import Game from './components/game/Game';
 import FormValidation from './components/about/Form';
 import { GameProvider } from './components/game/GameContext';
+import { StartProvider } from './components/home/StartContext';
 
 export default function App() {
   return (
     <GameProvider>
-      <div className="App">
-        <Router>
-          <div className="mainContainer">
-            <nav>
-              <div className="toggle">
-                <input type="checkbox" />
-                <span />
-                <span />
-                <span />
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/game">Game</Link>
-                  </li>
-                  <li>
-                    <Link to="/about">About</Link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/start" component={Start} />
-              <Route path="/game" component={Game} />
-              <Route path="/about">
-                <FormValidation />
-                <About />
-              </Route>
-              <Route exact path="/:id" component={PlanetsDetails} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <StartProvider>
+        <div className="App">
+          <Router>
+            <div className="mainContainer">
+              <nav>
+                <div className="toggle">
+                  <input type="checkbox" />
+                  <span />
+                  <span />
+                  <span />
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/game">Game</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/start" component={Start} />
+                <Route path="/game" component={Game} />
+                <Route path="/about">
+                  <FormValidation />
+                  <About />
+                </Route>
+                <Route exact path="/:id" component={PlanetsDetails} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </StartProvider>
     </GameProvider>
   );
 }
