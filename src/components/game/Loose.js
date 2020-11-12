d5b575c890b3db30d3b660b58b42aaed61262c03
Rom-mtl <romain.montel@gmail.com>
2020-11-12 17:08:00 +0100

    Design of game display and win/loose done.

--- src/components/game/Loose.js
+++ src/components/game/Loose.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import { GameContext } from './GameContext';
+import './winLoose.css';
 
 const Loose = () => {
   const { victory, handleClickReplay } = useContext(GameContext);
   return victory === '' ? (
     <div />
   ) : (
-    <div>
+    <div className="loose">
       <img src="./defeate.png" alt="T'as perdu" />
       <Button variant="contained" onClick={handleClickReplay} type="submit">
         Replay
