d9500b3a8041ef562b77ec5d6143855cef458920
AntoineGGG <a.gorenflot@gmx.fr>
2020-11-18 18:01:58 +0100

    fix css on rule character and game pages

--- src/components/planet/PlanetsDetails.js
+++ src/components/planet/PlanetsDetails.js
@@ -56,54 +56,68 @@ class PlanetDetails extends React.Component {
         <div className="caracsContainer">
           {carac.map((e) => (
             <div key={e.id} className="box">
-              <h2>{e.name}</h2>
+              <h1 className="planetsTitle">{e.name}</h1>
               <div className="planetsImg">
                 <img className="planetImg" src={e.url} alt={e.name} />
               </div>
               <div id="caracList">
                 <ul>
                   <li>
-                    <GiPathDistance size={70} /> : {e.fromSun} Millions km from
-                    sun
+                    <GiPathDistance size={70} />{' '}
+                    <span className="textDetails">
+                      {e.fromSun} Millions km from sun
+                    </span>
                   </li>
                   <br />
                   <li>
                     {' '}
                     <GiPlanetCore size={70} />
-                    Composition: {e.composition}
+                    <span className="textDetails">{e.composition}</span>
                   </li>
                   <br />
                   <li>
-                    <FaTemperatureLow size={70} /> Min: {e.tempMin}°
+                    <FaTemperatureLow size={70} />{' '}
+                    <span className="textDetails">Min: {e.tempMin}°</span>
                   </li>
                   <br />
                   <li>
-                    <FaTemperatureHigh size={70} /> Max: {e.tempMax}°
+                    <FaTemperatureHigh size={70} />{' '}
+                    <span className="textDetails">Max: {e.tempMax}°</span>
                   </li>
                   <br />
                   <li>
                     <RiSpeedLine size={70} />
-                    Vitesse de rotation: {e.rotation} km/h
+                    <span className="textDetails">
+                      rotation: {e.rotation} km/h
+                    </span>
                   </li>
                   <br />
                   <li>
                     <FiRotateCcw size={70} />
-                    Période de roration: {e.periodeRotation}h
+                    <span className="textDetails">
+                      Période de roration: {e.periodeRotation}h
+                    </span>
                   </li>
                   <br />
                   <li>
                     <RiSunLine size={70} />
-                    Révolution Solaire: {e.revolutionSolaire} jours
+                    <span className="textDetails">
+                      Révolution Solaire: {e.revolutionSolaire} jours
+                    </span>
                   </li>
                   <br />
                   <li>
                     <FaGrav size={70} />
-                    Gravity: {e.gravity} m/s²
+                    <span className="textDetails">
+                      Gravity: {e.gravity} m/s²
+                    </span>
                   </li>
                   <br />
                   <li>
                     <FaWeightHanging size={70} />
-                    Density: {e.density} g/cm3
+                    <span className="textDetails">
+                      Density: {e.density} g/cm3
+                    </span>
                   </li>
                   <br />
                   <li>{e.description}</li>
